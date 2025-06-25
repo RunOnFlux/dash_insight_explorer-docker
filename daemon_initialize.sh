9eb6664c6d6a0f98a8121f42395bfe168d6080e8
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2025-06-25 09:02:31 +0200

    Update daemon_initialize.sh

--- daemon_initialize.sh
+++ daemon_initialize.sh
@@ -90,7 +90,7 @@ else
   echo -e "${ARROW} ${YELLOW}Installing $COIN-node...${NC}"
   git clone $CORE_NODE_URL > /dev/null 2>&1
   cd $COIN-node
-  npm install --no-optional --legacy-peer-deps
+  npm install
   ./bin/$COIN-node create mynode
   cd mynode
   rm $COIN-node.json > /dev/null 2>&1
